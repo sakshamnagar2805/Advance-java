6ac23cd910b100d0b3add3f4ce1bfe40a9c8cd84
sakshamnagar2805 <sakshamngr2805@gmail.com>
2026-09-01 09:54:28 +0530

    Update App.java

--- JDBC/JDBC/src/App.java
+++ JDBC/JDBC/src/App.java
@@ -5,7 +5,7 @@ public class App {
        Connection con = DriverManager.getConnection(
         "jdbc:mysql://localhost:3306/college", //url
         "root", //user
-        "Shivam@1912#" //password
+        "Sam4213#" //password
        );
        System.out.println(con);
        System.out.println("connection created");
